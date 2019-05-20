1f0597580c4d8bec283519e238654d60c0abb398
Avitesh Kesharwani <itsavitesh@gmail.com>
2019-05-20 14:49:52 -0500

    spring batch initial project

---
+++ src/main/java/com/brownbag/springbatch/BatchMoverApplication.java
@@ -0,0 +1,13 @@
+package com.brownbag.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchMoverApplication.class, args);
+	}
+
+}
